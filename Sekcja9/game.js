74127f3b96c05ebe29329212d3d17e4462667010
Bartosz Hryniewicz <bartosz.hryniewicz3@gmail.com>
2020-04-18 01:06:41 +0200

    completely ended game rock paper scissors

--- Sekcja9/game.js
+++ Sekcja9/game.js
@@ -11,7 +11,7 @@ const game = {
     aiHand: ''
 }
 
-const hands = document.querySelectorAll('.select img');
+const hands = [...document.querySelectorAll('.select img')];
 
 //pierwsza funckja
 
@@ -21,4 +21,56 @@ function handSelection() {
     this.style.boxShadow = '0 0 0 4px cadetblue';
 }
 
+function aiChoice() {
+    const aiHand = hands[Math.floor(Math.random() * 3)].dataset.option;
+    return aiHand;
+}
+
+function checkResult(player, ai) {
+    if (player == ai) {
+        return 'draw'
+    } else if ((player === 'papier' && ai === 'kamień') || (player === 'kamien' && ai === 'nożyczki') || (player === 'nożyczki' && ai === 'papier')) {
+        return 'win'
+    } else {
+        return 'loss'
+    }
+}
+
+function publishResult(player, ai, result) {
+    document.querySelector('[data-summary="your-choice"]').textContent = player;
+    document.querySelector('[data-summary="ai-choice"]').textContent = ai;
+    document.querySelector('p.numbers span').textContent = ++gameSummary.numbers;
+
+    if (result === 'win') {
+        document.querySelector('p.wins span').textContent = ++gameSummary.wins;
+        document.querySelector('[data-summary="who-win"]').textContent = "Wygrales ty gnojku";
+        document.querySelector('[data-summary="who-win"]').style.color = 'green';
+    } else if (result === 'draw') {
+        document.querySelector('p.draws span').textContent = ++gameSummary.draws;
+        document.querySelector('[data-summary="who-win"]').textContent = "Remis pizdeczko";
+        document.querySelector('[data-summary="who-win"]').style.color = 'yellow';
+    } else {
+        document.querySelector('p.losses span').textContent = ++gameSummary.loses;
+        document.querySelector('[data-summary="who-win"]').textContent = "Przejebales co poradze";
+        document.querySelector('[data-summary="who-win"]').style.color = 'red';
+    }
+}
+
+function endGame() {
+    document.querySelector(`[data-option="${game.playerHand}"]`).style.boxShadow = '';
+    game.playerHand = '';
+}
+
+//funkcja sterujaca
+
+function startGame() {
+    if (!game.playerHand) return alert('wybierz opcje');
+    game.aiHand = aiChoice();
+    const gameResult = checkResult(game.playerHand, game.aiHand);
+    publishResult(game.playerHand, game.aiHand, gameResult);
+    endGame();
+}
+
 hands.forEach(hand => hand.addEventListener('click', handSelection));
+
+document.querySelector('.start').addEventListener('click', startGame);
